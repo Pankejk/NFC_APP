a4fbbc1f1d995d91d8a43a835f18ed81002b7179
Pankejk <mikelwatersky@gmail.com>
2015-01-24 13:12:41 +0100

    Druga wersja

--- NFC_APP/app/src/main/java/com/example/mike/nfc_app/ApduClass.java
+++ NFC_APP/app/src/main/java/com/example/mike/nfc_app/ApduClass.java
@@ -1,10 +1,13 @@
 package com.example.mike.nfc_app;
 
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.nfc.cardemulation.HostApduService;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 
+import java.io.UnsupportedEncodingException;
 import java.math.BigInteger;
 
 /**
@@ -12,18 +15,44 @@ import java.math.BigInteger;
  */
 
 public class ApduClass extends HostApduService {
+    /*private int counter =0;
+
+    @Override
+    public void onStart(Intent intent, int startId){
+        new Thread(new Runnable(){
+            @Override
+            public void run(){
+                while(true){
+                    Log.d("HCE", String.valueOf(counter));
+                    counter++;
+                    try{
+                        Thread.sleep(1000);
+                    }catch(InterruptedException e){
+                        e.printStackTrace();
+                    }
+                }
+            }
+        }).run();
+    }*/
     @Override
     public byte[] processCommandApdu(byte[] apdu, Bundle extras) {
         //String s = "3B8F8001804F0CA000000306030001000000006A";
         //BigInteger b = new BigInteger(s, 16);
         //byte [] array = b.toByteArray();
-        byte [] array = {(byte)0x3B,(byte)0x8F,(byte)0x80,0x01,(byte)0x80,0x4F,0x0C,(byte)0xA0,0x00,0x00,0x03,0x06,0x03,0x00,0x01,0x00,0x00,0x00,0x00,0x6A};
+        //byte [] array = {(byte)0x3B,(byte)0x8F,(byte)0x80,0x01,(byte)0x80,0x4F,0x0C,(byte)0xA0,0x00,0x00,0x03,0x06,0x03,0x00,0x01,0x00,0x00,0x00,0x00,0x6A};
         //byte [] array = {1,1,1,0,1,1,1,0,0,0,1,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,1,0,0,1,1,1,1,0,0,0,0,1,1,0,0,1,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,0,0,0,1,1,0,0,0,0,0,0,0,1,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,1,0,1,0};
         //byte [] array = {(byte)0x00}//,0x00, 0x00, 0x03,0x06};
-        return array;
+        byte[] response = null;
+        //String log = new String(apdu);
+        //Log.d(log, String.valueOf(0));
+        //if(apdu[2] == 0xA4) {
+            response = new byte[]{(byte) 0x90, 0x00};
+        //}
+        return response;
     }
 
     @Override
     public void onDeactivated(int reason) {
+        Log.d("HCE", "onDeactivated");
     }
 }
--- NFC_APP/app/src/main/java/com/example/mike/nfc_app/MainActivity.java
+++ NFC_APP/app/src/main/java/com/example/mike/nfc_app/MainActivity.java
@@ -2,17 +2,35 @@ package com.example.mike.nfc_app;
 
 //import android.support.v7.app.ActionBarActivity;
 import android.app.Activity;
+import android.app.PendingIntent;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.nfc.NfcAdapter;
+import android.nfc.Tag;
+import android.nfc.tech.MifareClassic;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import java.io.IOException;
 import java.math.BigInteger;
 
 
 public class MainActivity extends Activity {
     private TextView myText = null;
+    private static final byte[] HEX_CHAR_TABLE = { (byte) '0', (byte) '1',
+            (byte) '2', (byte) '3', (byte) '4', (byte) '5', (byte) '6',
+            (byte) '7', (byte) '8', (byte) '9', (byte) 'A', (byte) 'B',
+            (byte) 'C', (byte) 'D', (byte) 'E', (byte) 'F' };
+    //private NfcAdapter mAdapter = null;
+    //private PendingIntent mPendingIntent = null;
+    //private IntentFilter ndef = null;
+    //private IntentFilter [] mFilters = null;
+    //private String [][] mTechLists = null;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,54 +40,102 @@ public class MainActivity extends Activity {
         //LinearLayout lView = (LinearLayout)findViewById(R.id.textView);
 
         TextView textView = (TextView) findViewById(R.id.textView);
-        textView.setText("text you want to display\n HEHE\nHey guys, I know this is very basic but I need to know how I can display the contents of a variable on the screen.\n" +
-                "\n" +
-                "Do I use a textview in my layout?\n" +
-                "\n" +
-                "I have a textview box and I can set it to say something in the editor but I need to write the contents of a variable so I can do some error checking.\n" +
-                "\n" +
-                "Anyone help?Hey guys, I know this is very basic but I need to know how I can display the contents of a variable on the screen.\n" +
-                "\n" +
-                "Do I use a textview in my layout?\n" +
-                "\n" +
-                "I have a textview box and I can set it to say something in the editor but I need to write the contents of a variable so I can do some error checking.\n" +
-                "\n" +
-                "Anyone help?Hey guys, I know this is very basic but I need to know how I can display the contents of a variable on the screen.\n" +
-                "\n" +
-                "Do I use a textview in my layout?\n" +
-                "\n" +
-                "I have a textview box and I can set it to say something in the editor but I need to write the contents of a variable so I can do some error checking.\n" +
-                "\n" +
-                "Anyone help?Hey guys, I know this is very basic but I need to know how I can display the contents of a variable on the screen.\n" +
-                "\n" +
-                "Do I use a textview in my layout?\n" +
-                "\n" +
-                "I have a textview box and I can set it to say something in the editor but I need to write the contents of a variable so I can do some error checking.\n" +
-                "\n" +
-                "Anyone help?Hey guys, I know this is very basic but I need to know how I can display the contents of a variable on the screen.\n" +
-                "\n" +
-                "Do I use a textview in my layout?\n" +
-                "\n" +
-                "I have a textview box and I can set it to say something in the editor but I need to write the contents of a variable so I can do some error checking.\n" +
-                "\n" +
-                "Anyone help?Hey guys, I know this is very basic but I need to know how I can display the contents of a variable on the screen.\n" +
-                "\n" +
-                "Do I use a textview in my layout?\n" +
-                "\n" +
-                "I have a textview box and I can set it to say something in the editor but I need to write the contents of a variable so I can do some error checking.\n" +
-                "\n" +
-                "Anyone help?Hey guys, I know this is very basic but I need to know how I can display the contents of a variable on the screen.\n" +
-                "\n" +
-                "Do I use a textview in my layout?\n" +
-                "\n" +
-                "I have a textview box and I can set it to say something in the editor but I need to write the contents of a variable so I can do some error checking.\n" +
-                "\n" +
-                "Anyone help?");
-        ApduClass tmp = new ApduClass();
-        byte [] array =null;
-        Bundle bu = null;
-        tmp.processCommandApdu(array, bu);
+        textView.setText("asda");
+        NfcAdapter mAdapter =NfcAdapter.getDefaultAdapter(this);
+        PendingIntent mPendingIntent = PendingIntent.getActivity(this, 0,
+                new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
+        IntentFilter ndef = new IntentFilter(NfcAdapter.ACTION_TECH_DISCOVERED);
+        IntentFilter [] mFilters = new IntentFilter[] {ndef,};
+        String [] [] mTechLists = new String[][] { new String[] { MifareClassic.class.getName() } };
+
+        try {
+            ndef.addDataType("*/*");
+        } catch (IntentFilter.MalformedMimeTypeException e) {
+            throw new RuntimeException("fail", e);
+        }
+
+        Log.i("TAG","COS dZiala");
+        Intent intent = getIntent();
+        resolveIntent(intent);
+        //ApduClass tmp = new ApduClass();
+        //byte [] array =null;
+        //Bundle bu = null;
+        //tmp.processCommandApdu(array, bu);
     }
+
+    public void resolveIntent(Intent intent){
+        String action = intent.getAction();
+        Log.i("TAG","COS DZIALA");
+        Log.i("TAG",action);
+        if(NfcAdapter.ACTION_TECH_DISCOVERED.equals(action)){
+            Tag tagFromIntent = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
+            MifareClassic mfc = MifareClassic.get(tagFromIntent);
+            byte [] data;
+
+            try{
+                Log.i("TAG","A TO DZIALA?fffffffffff");
+                mfc.connect();
+                Log.i("TAG","A TO DZIALA?lllll");
+                boolean auth = false;
+                String cardData = null;
+                int secCount = mfc.getSectorCount();
+                int bCount =0;
+                int bIndex = 0;
+                for(int j=0; j<secCount;j++){
+                    auth = mfc.authenticateSectorWithKeyA(j,MifareClassic.KEY_DEFAULT);
+                    if(auth){
+                       bCount = mfc.getBlockCount();
+                        bIndex = 0;
+                        for(int i=0; i<bCount; i++){
+                            bIndex = mfc.sectorToBlock(j);
+                            data = mfc.readBlock(bIndex);
+                            Log.i("TAG",getHexString(data,data.length));
+                            bIndex++;
+                        }
+                    }else{
+                        Log.e("tag","Autoryzacja nie powiodla sie!\n");
+                    }
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        Log.i("TAG","A TO DZIALASDASFDAFASF");
+    }
+
+    public static String getHexString(byte[] raw, int len){
+        byte [] hex = new byte[2*len];
+        int index =0;
+        int pos =0;
+        for(byte b : raw){
+            if(pos>=len)
+                break;
+            pos++;
+            int v = b & 0xFF;
+            hex[index++] = HEX_CHAR_TABLE[v >>> 4];
+            hex[index++] = HEX_CHAR_TABLE[v & 0xF];
+        }
+        return new String(hex);
+    }
+
+
+    /*@Override
+    public void onResume() {
+        super.onResume();
+        mAdapter.enableForegroundDispatch(this, mPendingIntent, mFilters, mTechLists);
+    }
+
+    @Override
+    public void onNewIntent(Intent intent) {
+        Log.i("Foreground dispatch", "Discovered tag with intent: " + intent);
+        resolveIntent(intent);
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mAdapter.disableForegroundDispatch(this);
+    }*/
 
 
     @Override
